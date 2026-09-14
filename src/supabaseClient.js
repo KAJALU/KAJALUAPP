2ef4283ca9aa5cd077c04038691b5958bb065e2c
KAJALU <kajaluapp@gmail.com>
2026-09-14 07:17:35 -0500

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Datos de conexión del proyecto "KajaluApp" en Supabase.
+// Datos de conexión del proyecto "KajaluApp2" en Supabase.
 // La llave "publishable" está diseñada para usarse en el navegador (no es secreta).
-const supabaseUrl = "https://xeugcyedyezxtkcywlilq.supabase.co";
-const supabaseKey = "sb_publishable_tJJFYatKD0CkgouZn5SLsA__aymW4fL";
+const supabaseUrl = "https://txghoregizndbneuosvp.supabase.co";
+const supabaseKey = "sb_publishable_A6YESZjTN3pfFqfUyvjEgQ_IqtVnl2Y";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
